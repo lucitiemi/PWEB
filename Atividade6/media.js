9e3c35e32f25890ae1274bfc9cd782a9337ffe4e
Luciana <lt.shimoyama@gmail.com>
2025-03-24 12:26:17 -0300

    Upload Atividade 06

---
+++ Atividade6/media.js
@@ -0,0 +1,10 @@
+var nome = prompt("Qual o seu nome?");
+
+var n1 = parseFloat(prompt("Nota 1:"));
+var n2 = parseFloat(prompt("Nota 2:"));
+var n3 = parseFloat(prompt("Nota 3:"));
+var n4 = parseFloat(prompt("Nota 4:"));
+
+var media = (n1+n2+n3+n4)/4;
+
+alert("Media = " + media);
---
+++ Atividade6/operacoes.js
@@ -0,0 +1,14 @@
+var n1 = parseFloat(prompt("Numero 1:"));
+var n2 = parseFloat(prompt("Numero 2:"));
+
+var soma = n1+n2;
+var sub = n1-n2;
+var prod = n1*n2;
+var div = n1/n2;
+var rest = n1%n2;
+
+alert("Soma = " + soma);
+alert("Subtração = " + sub);
+alert("Produto = " + prod);
+alert("Divisão = " + div);
+alert("Resto = " + rest);
